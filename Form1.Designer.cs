743b1f3953c90b455d9b79e6aea7028e4f9a90e5
kdbajj <k.dbaj@vp.pl>
2022-03-29 23:18:53 +0200

    Add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ListaZakupów
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.btnUsun = new System.Windows.Forms.Button();
+            this.btnWyczysc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.listBox1.ForeColor = System.Drawing.Color.Black;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Location = new System.Drawing.Point(140, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 410);
+            this.listBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.progressBar1.Location = new System.Drawing.Point(140, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(296, 22);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 456);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnDodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnDodaj.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDodaj.Location = new System.Drawing.Point(140, 483);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(296, 50);
+            this.btnDodaj.TabIndex = 3;
+            this.btnDodaj.Text = "Dodaj";
+            this.btnDodaj.UseVisualStyleBackColor = false;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // btnUsun
+            // 
+            this.btnUsun.BackColor = System.Drawing.Color.White;
+            this.btnUsun.FlatAppearance.BorderSize = 0;
+            this.btnUsun.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnUsun.Location = new System.Drawing.Point(140, 539);
+            this.btnUsun.Name = "btnUsun";
+            this.btnUsun.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnUsun.Size = new System.Drawing.Size(140, 44);
+            this.btnUsun.TabIndex = 4;
+            this.btnUsun.Text = "Usuń";
+            this.btnUsun.UseVisualStyleBackColor = false;
+            this.btnUsun.Click += new System.EventHandler(this.btnUsun_Click);
+            // 
+            // btnWyczysc
+            // 
+            this.btnWyczysc.BackColor = System.Drawing.Color.White;
+            this.btnWyczysc.FlatAppearance.BorderSize = 0;
+            this.btnWyczysc.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.btnWyczysc.Location = new System.Drawing.Point(296, 539);
+            this.btnWyczysc.Name = "btnWyczysc";
+            this.btnWyczysc.Size = new System.Drawing.Size(140, 44);
+            this.btnWyczysc.TabIndex = 5;
+            this.btnWyczysc.Text = "Wyczyść";
+            this.btnWyczysc.UseVisualStyleBackColor = false;
+            this.btnWyczysc.Click += new System.EventHandler(this.btnWyczysc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = global::ListaZakupów.Properties.Resources._3922d94c0a462e402d2e2cf7e39a1a0f;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(566, 651);
+            this.Controls.Add(this.btnWyczysc);
+            this.Controls.Add(this.btnUsun);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Lista zakupów";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Button btnUsun;
+        private System.Windows.Forms.Button btnWyczysc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaZakupów
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+            {
+                if (textBox1.Text.Length > 0)
+                {
+                     if (listBox1.Items.Contains(textBox1.Text))
+                    {
+                        DialogResult result = MessageBox.Show("Ten element już istnieje, chcesz go dodać?", "Uwaga!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);//dialog result sluzy do przechowania informacji, ostrzeżenie o istniejącym elemencie
+                        if (result == DialogResult.No)// jeśli użytkownik naciśnie "no"
+                        {
+                            return;
+                        }
+                    }
+                    listBox1.Items.Add(textBox1.Text);//jesli lista ma jakies wartości, dodaj wpisaną wartość
+                    aktualizujListę();//funkcja, która służy do aktualizowania wskaźnika
+                    textBox1.Clear();//czyszczenie pola do wpisywania
+                }
+                else
+                {
+                    MessageBox.Show("Nie podano żadnej wartości!","Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lista jest już pełna!", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        private void aktualizujListę()
+        {
+            int ilość = listBox1.Items.Count;
+            progressBar1.Value = ilość * 10;
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+            int liczbaIndeksów = listBox1.SelectedIndex;
+            if (liczbaIndeksów != -1)
+            {
+                listBox1.Items.RemoveAt(liczbaIndeksów);
+            }
+            else
+            {
+                MessageBox.Show("Żaden element nie został zaznaczony!", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnWyczysc_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            aktualizujListę();
+
+        }
+    }
+}
